15cce25cb61041690d83c87ae276c58f9e261883
Rissmon <rissmon@gmail.com>
2021-11-24 02:21:39 +0530

    [Test Case] Initial screen should be game board fragment when the application launched.

---
+++ app/src/androidTest/java/com/samples/test/MainActivityTest.kt
@@ -0,0 +1,25 @@
+package com.samples.test
+
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.core.app.launchActivity
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.samples.test.view.MainActivity
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest {
+
+    private val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+    private lateinit var activityScenario: ActivityScenario<MainActivity>
+
+    @Test
+    fun activityNavigation_initialScreenShouldBeGameFragment() {
+        activityScenario = launchActivity()
+        activityScenario.onActivity { navController.setGraph(R.navigation.nav_graph) }
+        assertEquals(navController.currentDestination?.id, R.id.gameFragment)
+    }
+}
---
+++ app/src/main/java/com/samples/test/view/game/GameBoardFragment.kt
@@ -0,0 +1,6 @@
+package com.samples.test.view.game
+
+import androidx.fragment.app.Fragment
+import com.samples.test.R
+
+class GameBoardFragment : Fragment(R.layout.fragment_board)
