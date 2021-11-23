ca98c2a0bdfb0bb0a2f41bd94f360b74027dd2b6
Rissmon <rissmon@gmail.com>
2021-11-23 20:55:36 +0530

    Code Refactor.
    Reused common methods in GameManagerImplTest.kt

--- app/src/test/java/com/samples/test/GameManagerImplTest.kt
+++ app/src/test/java/com/samples/test/GameManagerImplTest.kt
@@ -17,9 +17,9 @@ class GameManagerImplTest {
     fun getInitialBoard_willReturnBoardWithAllTheCellsInUnSelectedState() = runBlockingTest {
         val expectedCellSize: Int = BOARD_SIZE * BOARD_SIZE
 
-        val board = gameManager.getBoard().first()
+        val board = getBoard()
 
-        val actualCellSize = board.cells.filter { it.state == UnSelected }.size
+        val actualCellSize =  getCellSize(board.cells, UnSelected)
         assertEquals(expectedCellSize, actualCellSize)
     }
 
